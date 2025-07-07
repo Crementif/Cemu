192e3d98a3ad1c4ee17afbc97f5e9604a80fa02c
Crementif <26669564+Crementif@users.noreply.github.com>
2025-07-07 23:18:24 +0200

    UI: Update PPC debugger to support dark mode
    Might have to tweak the dark mode colors later if I notice some of them are odd choices.

--- src/gui/debugger/DumpWindow.cpp
+++ src/gui/debugger/DumpWindow.cpp
@@ -2,7 +2,6 @@
 #include "gui/debugger/DumpWindow.h"
 
 #include "gui/debugger/DebuggerWindow2.h"
-#include "Cafe/HW/Espresso/Debugger/Debugger.h"
 #include "gui/debugger/DumpCtrl.h"
 
 enum
@@ -17,8 +16,6 @@ enum
 DumpWindow::DumpWindow(DebuggerWindow2& parent, const wxPoint& main_position, const wxSize& main_size)
 	: wxFrame(&parent, wxID_ANY, _("Memory Dump"), wxDefaultPosition, wxSize(600, 250), wxSYSTEM_MENU | wxCAPTION | wxCLIP_CHILDREN | wxRESIZE_BORDER | wxFRAME_FLOAT_ON_PARENT)
 {
-	this->wxWindowBase::SetBackgroundColour(*wxWHITE);
-
 	wxBoxSizer* main_sizer = new wxBoxSizer(wxVERTICAL);
 	m_dump_ctrl = new DumpCtrl(this, wxID_ANY, wxDefaultPosition, wxDefaultSize, wxScrolledWindowStyle);
 	main_sizer->Add(m_dump_ctrl, 1, wxEXPAND);
