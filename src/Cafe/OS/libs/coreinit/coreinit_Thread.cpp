4422bb8293552ce51622e002de014d80f9217c89
Crementif <26669564+Crementif@users.noreply.github.com>
2026-09-05 22:50:06 +0200

    arm64: fix flush-to-zero behaviour for non-apple ARM PCs
    __arm64 is only something that Apple provides. Meanwhile we do want to change this setting for Windows or Linux ARM PCs too.

--- src/Cafe/OS/libs/coreinit/coreinit_Thread.cpp
+++ src/Cafe/OS/libs/coreinit/coreinit_Thread.cpp
@@ -15,7 +15,7 @@
 
 #include "util/helpers/helpers.h"
 
-#ifdef __arm64__
+#if defined(__aarch64__) || defined(_M_ARM64)
 #if defined(__clang__)
 #include <arm_acle.h>
 #elif defined(_MSC_VER)
@@ -29,7 +29,7 @@ void enableFlushDenormalsToZero()
 {
 #if defined(ARCH_X86_64)
 	_mm_setcsr(_mm_getcsr() | 0x8000);
-#elif defined(__arm64__)
+#elif defined(__aarch64__) || defined(_M_ARM64)
 #if defined(__clang__)
 	__arm_wsr64("fpcr", __arm_rsr64("fpcr") | (1 << 24));
 #elif defined(__GNUC__)
